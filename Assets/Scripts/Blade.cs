bab524eb70859484af04a3f437556c2046e5afed
lara <laralara133@gmail.com>
2023-12-04 17:01:03 +0300

    Blade adjusted for mobile play

--- Assets/Scripts/Blade.cs
+++ Assets/Scripts/Blade.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class Blade : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public float minVelocity = 0.1f;
+    private Vector3 lastMousePos;
+    private Vector3 mouseVelocity;
+    private new Collider2D collider;
+
+
     // Awake is called before the first frame update and before the start methods
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>(); //Blade object has the Blade script, and also has a 2d collider
+        //Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        collider.enabled = isMouseMoving();
         SetBladeToMouse();
     }
 
@@ -23,5 +32,21 @@ public class Blade : MonoBehaviour
         var mousePos = Input.mousePosition;
         mousePos.z = 10; //position adjusted for the z position of fruits
         rb.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+
+    private bool isMouseMoving()
+    {
+        Vector3 currentMousePos = transform.position;
+        float traveled = (lastMousePos - currentMousePos).magnitude;
+
+        lastMousePos = currentMousePos;
+        if(traveled > minVelocity)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
